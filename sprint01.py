673fa1e812980666b6febe0f62624de7c8d33713
tmitche2 <36667814+tmitche2@users.noreply.github.com>
2019-03-04 13:21:39 -0500

    Update sprint01.py

--- sprint01.py
+++ sprint01.py
@@ -1,4 +1,4 @@
-#How I ran/tested everything w/ the parser
+#How I tested everything w/ the parser for both US01 and US02. Should compile and run properly if downloaded. Using test0.ged for testing
 import datetime
 individuals = []
 families = []
@@ -166,6 +166,44 @@ def monthsplit(date):
           temp[0] = '0' + temp[0]
         stringgs = str(temp[2]) + '-' + str(temp[1]) + '-' + str(temp[0])
         return stringgs
+
+#US01, dates before today
+dat = datetime.datetime.strptime(str(datetime.date.today()), '%Y-%m-%d')
+formattedDate = dat.strftime('%Y-%b-%d')
+validDates = True
+def datesBeforeToday(indData, famData): #Dates: birth, death, marriage, divorce
+    individuals.sort(key=lambda x: int(x.i_id[1:]))
+    families.sort(key=lambda x: int(x.f_id[1:]))
+    for ind in indData:
+        b = monthsplit(ind.get_birth())
+        if (b != None):
+            if (formattedDate < b):
+                print(ind.get_name() + " born before current date. " + ind.get_birth())
+                validDates = False
+        if (ind.get_death() != "NA"):
+            d = monthsplit(ind.get_death())
+            if (formattedDate < d):
+               print(ind.get_name() + " died before current date.")
+               validDates = False
+    for fam in famData:
+        wifename = individuals[int(fam.get_wife()[1:]) - 1].get_name()
+        hubbyname = individuals[int(fam.get_husband()[1:]) - 1].get_name()
+        m = monthsplit(fam.get_marriage())
+        if (m != None):
+            if (formattedDate < m):
+                print(hubbyname + " " + wifename + " married before current date.")
+                validDates = False
+        if (fam.get_divorce() != "NA"):
+            dev = monthsplit(fam.get_divorce())
+            if (formattedDate < dev):
+                print(hubbyname + " " + wifename + " divorced before current date.")
+                validDates = False
+    if(validDates == True):
+        print("All dates are valid in this GEDCOM file.")
+    else:
+        print("Not all dates are valid in this GEDCOM file.")
+
+#US02, birth before marriage
 dates = []
 validMarriage = True
 def birthBeforeMarriage(indList, famData):
@@ -222,6 +260,7 @@ def main():
     print_individuals()
     #print()
     print_families()
+    datesBeforeToday(individuals, families)
     birthBeforeMarriage(individuals, families)
     
 if __name__ == '__main__':

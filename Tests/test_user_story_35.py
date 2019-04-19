7c1eb223c5077c2e1f7e237869933b9091ceb10b
SecondThrow <malexan0@gmail.com>
2019-04-18 20:55:32 -0400

    Finished converting remaining tests based on tabulate

--- Tests/test_user_story_35.py
+++ Tests/test_user_story_35.py
@@ -27,20 +27,28 @@ class TestUserStory35(unittest.TestCase):
 
     def test_empty_file(self):
         individuals.clear()
-        self.assertFalse(list_recent_births())
+        table = []
+        list_recent_births(table)
+        self.assertFalse(table[0][4].split()) and self.assertTrue(table[0][3])
 
     def test_mixed_births(self):
-        self.assertCountEqual([get_individual("I1"), get_individual("I4")], list_recent_births())
+        table = []
+        list_recent_births(table)
+        self.assertCountEqual([get_individual("I1").name, get_individual("I4").name], table[0][4].split("\n")) and self.assertTrue(table[0][3])
 
     def test_all_recent_births(self):
         get_individual("I2").birth = datetime.strptime("19 AUG 2018", "%d %b %Y").date()
         get_individual("I3").birth = datetime.strptime("19 AUG 2018", "%d %b %Y").date()
-        self.assertCountEqual(individuals, list_recent_births())
+        table = []
+        list_recent_births(table)
+        self.assertCountEqual([get_individual("I1").name, get_individual("I2").name, get_individual("I3").name, get_individual("I4").name], table[0][4].split("\n")) and self.assertTrue(table[0][3])
 
     def test_all_far_births(self):
         get_individual("I1").birth = datetime.strptime("20 AUG 1998", "%d %b %Y").date()
         get_individual("I4").birth = datetime.strptime("20 AUG 1998", "%d %b %Y").date()
-        self.assertFalse(list_recent_births())
+        table = []
+        list_recent_births(table)
+        self.assertFalse(table[0][4].split()) and self.assertTrue(table[0][3])
 
 
 if __name__ == "__main__":
